ea25831d4f1e88068ea4b9abfd8994cdbbc0ab92
sg111043 <sayan.ghosh@maxlifeinsurance.com>
2023-11-16 17:40:49 +0530

    first commit

---
+++ src/test/java/com/sayanpublic/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayanpublic.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
